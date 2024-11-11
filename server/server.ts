a837de2be78bb98d5ff04a4f48e2ab6939f72abf
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-11-11 13:12:41 +0000

    rediseño del server.ts

--- server/server.ts
+++ server/server.ts
@@ -3,10 +3,16 @@ import express from "express";
 import cors from "cors";
 import { connectToDatabase } from "./database";
 
-// Load environment variables from the .env file, where the ATLAS_URI is configured
-dotenv.config();
-
 const { ATLAS_URI } = process.env;
+const app = express();
+
+// Settings 
+dotenv.config();
+app.set('port', process.env.PORT || 5200);
+
+// Middleware
+
+// Routes
 
 if (!ATLAS_URI) {
   console.error(
@@ -17,12 +23,12 @@ if (!ATLAS_URI) {
 
 connectToDatabase(ATLAS_URI)
   .then(() => {
-    const app = express();
+    // const app = express();
     app.use(cors());
 
     // start the Express server
-    app.listen(5200, () => {
-      console.log(`Server running at http://localhost:5200...`);
+    app.listen(app.get('port'), () => {
+      console.log(`Server running at http://localhost:`, app.get('port'));
     });
   })
   .catch((error) => console.error(error));
