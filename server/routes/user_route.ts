d4b9bad77012c1cb10dc8d2733c88f01d63184f0
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-11-12 19:09:40 +0000

    register y login operativos

--- server/routes/user_route.ts
+++ server/routes/user_route.ts
@@ -15,13 +15,39 @@ router.get("/", async (req: Request, res: Response) => {
   }
 });
 
-router.post("/", async (req: Request, res: Response) => {
+// FIXME:analizar si name password y role son necesarios
+router.post("/register", async (req: Request, res: Response) => {
   try {
+    const { email } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'Email alrady registered' });
+    }
     const new_user = new User(req.body);
     await new_user.save();
     res.status(201).json({
       message: "User created successfully"
   });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+});
+
+router.post("/login", async (req: Request, res: Response) => {
+  try {
+    const { email } = req.body;
+    console.log(email);
+    const reqUser = await User.findOne({ email });
+    console.log(reqUser?.email);
+    if (reqUser?.email !== email) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({
+      message: 'Login exitoso'
+    });
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error('Error:', error.message);
