92814277b12987546459879bd8b81977910b59ae
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-11-12 15:23:51 +0000

    modelo User actulizado con validaciones

--- server/models/user_model.ts
+++ server/models/user_model.ts
@@ -1,24 +1,40 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+// const mongoose = require('mongoose');
+// const { Schema } = mongoose;
 
-const User = new Schema({
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface UserInterface extends Document {
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+}
+
+const UserSchema: Schema = new Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'The name of the user is required.'],
+        minlength: [3, 'The minimum length is 3 characters'],
+        maxlength: [20, 'The maximum length is 20 characters'],
     },
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, 'The email is required.'],
+        unique: true,
+        match: [ /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            'El correo electrónico no es válido'],
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'The password is required.']
     },
     role: {
         type: String,
+        enum: ['student', 'college', 'teacher', 'master'],
         default: 'student'
     }
 });
 
-module.exports = mongoose.model('User', User);
+// module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model<UserInterface>('User', UserSchema);
+export default User;
