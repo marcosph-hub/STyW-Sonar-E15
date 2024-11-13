1ca7134ff4483632f8c1e5a873318263ba51a949
alu0101316734 <alu0101316734@ull.edu.es>
2024-11-13 18:39:00 +0000

    add subject_model

---
+++ server/models/subject_model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface SubjectInterface extends Document {
+    name: string;
+    type_education: string;
+    description:string;
+}
+
+const SubjectSchema:Schema = new Schema({
+    name:{
+        type:String,
+        required: [true, 'The name of the subject is required.'],
+        minlength: [3, 'The minimum length is 3 characters'],
+        maxlength: [20, 'The maximum length is 20 characters'],        
+    },
+    type_education:{
+        type:String,
+        required: [true, 'The type of education is required.'],
+        enum:['School','High School','University']
+    },
+    description:{
+        type:String,
+        required: [true, 'Description is required.'],
+    }
+})
+
+const Subject = mongoose.model<SubjectInterface>('Subject', SubjectSchema);
