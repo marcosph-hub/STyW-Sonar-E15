5e4575afee382335f7da81ef8bcfbab55c20d707
alu0101316734 <alu0101316734@ull.edu.es>
2024-11-13 20:01:55 +0000

    add chronometer_model

--- server/models/method_study.ts
+++ server/models/method_study.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, Document,Types } from 'mongoose';
+import mongoose, { Schema, Document} from 'mongoose';
 
 interface MethodStudyInterface extends Document {
     name:string;
