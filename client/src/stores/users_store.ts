1a5e9cdd079f1a3bfcfc845c8f2b32c35337d648
alu0101316734 <alu0101316734@ull.edu.es>
2024-11-14 18:39:41 +0000

    dara model with api conection users

--- client/src/stores/users_store.ts
+++ client/src/stores/users_store.ts
@@ -16,30 +16,56 @@ export const useUsersStorre = defineStore('User',()=>{
         user.value= data.map((user:any)=> new User(user.user_name,user.email,user.password,user.role,user._id))
     }
     async function addUser(useradd:UsersInterfaces) {
-        user.value.push(useradd)
-    }
+      let port:number | string= process.env.PORT || 5300;
+      try{
+      const response = await fetch('http://localhost/register:'+ port, 
+       { method: 'POST', headers: 
+       { 'Content-Type': 'application/json' }, 
+          body: JSON.stringify(useradd), 
+       });
+        const add_user=await response.json();
+        user.value.push(add_user)
+        }catch{
+          console.error('Issue in add users')
+        }
+      }
     async function deleteUser(id: Types.ObjectId) 
     { 
+        let port:number | string= process.env.PORT || 5300;
         const index = user.value.findIndex(usuario => {if(usuario._id)usuario._id.equals(id)}); 
         if (index !== -1) 
-        { 
+        {       
+           const response = await fetch('http://localhost/'+ id +':'+ port, 
+           { method: 'DELETE', headers: 
+           { 'Content-Type': 'application/json' }
+           });
           user.value.splice(index, 1); 
         } 
         else 
         { 
-           console.error("Usuario no encontrado"); 
+           console.error("User no found"); 
         } 
     }
-    function putUser(id: Types.ObjectId, datosActualizados: Partial<UsersInterfaces>) 
+    async function putUser(id: Types.ObjectId, datosActualizados: Partial<UsersInterfaces>) 
       { 
+        try{
+        let port:number | string= process.env.PORT || 5300;
         const index = user.value.findIndex(usuario => usuario._id?.equals(id)); 
          if (index !== -1) 
          {  
+          const response = await fetch('http://localhost/'+ id +':'+ port, 
+          { method: 'PUT', headers: 
+          { 'Content-Type': 'application/json' }, 
+             body: JSON.stringify(datosActualizados), 
+          })
             user.value[index] = { ...user.value[index], ...datosActualizados }; } 
          else 
          { 
-           console.error("Usuario no encontrado"); 
+           console.error("User no found"); 
          } 
+         } catch{
+          console.error("User no found"); 
+         }
     }
     return {user,addUser,getUsers,putUser,deleteUser}
 })
