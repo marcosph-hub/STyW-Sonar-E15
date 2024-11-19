2cbd3ffad24a1fc19c6a6d78812bc6632072ff25
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-11-19 20:45:14 +0000

    Register view structure

---
+++ client/src/views/RegisterView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue"
+import RegisterUser from "@/components/Register_User.vue"
+</script>
+
+<template>
+    <main>
+        <div>
+            <Navbar />
+            <RegisterUser />
+        </div>
+    </main>
+</template>
