50b1e2f4b037da22a3468ca59da39368ee8b1e6a
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-11-19 16:50:20 +0000

    register component init

--- client/src/components/UserRegister.vue
+++ client/src/components/UserRegister.vue
@@ -1,43 +1,13 @@
-<template>
-  <div>
-    <h2>Registrar Usuario</h2>
-    <form @submit.prevent="registerUser">
-      <input v-model="name" placeholder="Nombre" required />
-      <input v-model="email" placeholder="Correo" required type="email" />
-      <input v-model="password" placeholder="Contraseña" required type="password" />
-      <select v-model="role">
-        <option value="student">Student</option>
-        <option value="teacher">Teacher</option>
-        <option value="master">Master</option>
-      </select>
-      <button type="submit">Registrar</button>
-    </form>
-  </div>
-</template>
-
-<script lang="ts">
-import { defineComponent, ref } from 'vue'
-import { useUserStore } from '@/stores/userStore'
-
-export default defineComponent({
-  setup() {
-    const userStore = useUserStore()
-
-    const name = ref('')
-    const email = ref('')
-    const password = ref('')
-    const role = ref('student')
-
-    function registerUser() {
-      userStore.registerUser({
-        name: name.value,
-        email: email.value,
-        password: password.value,
-        role: role.value,
-      })
-    }
-
-    return { name, email, password, role, registerUser }
-  },
-})
+<script setup lang = "ts">
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 </script>
+<template>
+  <header>
+
+  </header>
+
+  <body>
+    
+  </body>
+</template>
