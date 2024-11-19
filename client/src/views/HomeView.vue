bd4d791bdf00efffe9d9e87adc240de074afb59e
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-11-19 20:42:54 +0000

    Home View

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
 import Navbar from "@/components/Navbar.vue"
+import { ref, onMounted } from 'vue'; 
+const show = ref(false); 
+onMounted(() => 
+{ 
+  show.value = true; 
+});
 </script>
+
 
 <template>
   <main>
     <Navbar/>
+    <transition name="slide"> 
+      <div v-if="show" class="container"> 
+        <h1>Bienvenido</h1> 
+        <p>Esta es tu página ideal para estudiar.</p> 
+      </div> 
+    </transition>
   </main>
 </template>
