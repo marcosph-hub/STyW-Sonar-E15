44c38bcc6548fc58703908bb1ad19df08bb8fc7a
Marcos Padilla Herrera <marcosph98@gmail.com>
2024-11-28 23:04:24 +0000

    user method preference model to join user model and study method

---
+++ server/models/userPreferencesModel.ts
@@ -0,0 +1,30 @@
+import { Schema, model, Types } from "mongoose";
+
+interface UserPreferencesInterface extends Document {
+    userId: Types.ObjectId;
+    preferedMethod: Types.ObjectId;
+    customSettings?: {
+        workDuration?: number;
+        breakDuration?: number;
+    };
+}
+
+const UserPreferencesSchema = new Schema<UserPreferencesInterface>({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    preferedMethod: {
+        type: Schema.Types.ObjectId,
+        ref: 'StudyMethodModel',
+        required: true
+    },
+    customSettings: {
+        workDuration: { type: Number },
+        breakDuration: { type: Number }
+    }
+});
+
+const UserPreferencesModel = model<UserPreferencesInterface>('UserPreferencesModel', UserPreferencesSchema);
+export default UserPreferencesModel;
