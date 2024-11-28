ce452863ed942875814b64745cb71e25b38ae3fa
Marcos Padilla Herrera <marcosph98@gmail.com>
2024-11-28 20:38:23 +0000

    jest config file

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts'],
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['**/*.test.ts', 'ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
+
+  
