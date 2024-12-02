c25c60241184bd69f291c6debbe58c44003c636f
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-12-02 22:53:09 +0000

    navbar styles fixed

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
   <header class="menu-bar">
     <div class="menu-container">
-      <nav class="menu"> 
-          <ul> 
-            <li><img src="./../assets/SM_logo.png" alt="Method Study Logo" class="logo" /></li>
-            <li><h1 class="logo-text">Method Study</h1></li>
-            <li><router-link to="/">Inicio</router-link></li>
-            <li><router-link to="/about">Acerca de </router-link></li>
-            <li><a href="#contacto">Contacto</a></li> 
-            <div class="auth-buttons">
+      <nav class="menu">
+        <ul>
+          <li><img src="./../assets/SM_logo.png" alt="Method Study Logo" class="logo" /></li>
+          <li><h1 class="logo-text">Method Study</h1></li>
+          <li><router-link to="/">Inicio</router-link></li>
+          <li><router-link to="/about">Acerca de </router-link></li>
+          <li><a href="#contacto">Contacto</a></li>
+          <div class="auth-buttons">
             <ul>
               <!-- Si el usuario está autenticado -->
               <li v-if="auth.isAuthenticated">
@@ -26,54 +26,48 @@
               </li>
             </ul>
           </div>
-
         </ul>
       </nav>
-
     </div>
   </header>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
-import { useAuthStore } from "@/stores/authstore"; // Tu store de autenticación
-import { useRouter } from "vue-router";
+import { defineComponent } from 'vue'
+import { useAuthStore } from '@/stores/authstore' // Tu store de autenticación
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
-  name: "Navbar",
+  name: 'Navbar',
   setup() {
-    const auth = useAuthStore(); // Accede al store de autenticación
-    const router = useRouter(); // Router para navegar entre vistas
-
+    const auth = useAuthStore() // Accede al store de autenticación
+    const router = useRouter() // Router para navegar entre vistas
 
     const handleLogin = () => {
-      router.push("/login"); // Redirige a la página de inicio de sesión
-    };
-
+      router.push('/login') // Redirige a la página de inicio de sesión
+    }
 
     const handleRegister = () => {
-      router.push("/register"); // Redirige a la página de registro
-    };
-
+      router.push('/register') // Redirige a la página de registro
+    }
 
     const handleLogout = async () => {
-      await auth.logout(); // Cierra sesión a través del store
-      router.push("/"); // Redirige al inicio después de cerrar sesión
-    };
-
+      await auth.logout() // Cierra sesión a través del store
+      router.push('/') // Redirige al inicio después de cerrar sesión
+    }
 
     return {
       auth, // Acceso al estado reactivo del store
       handleLogin,
       handleRegister,
       handleLogout,
-    };
+    }
   },
-});
+})
 </script>
 
 <style scoped>
 .menu-bar {
-  background-color: #344E41;
+  background-color: #344e41;
   color: rgba(7, 255, 139, 0.308);
   padding: 10px 20px;
   position: fixed;
@@ -85,7 +79,6 @@ export default defineComponent({
 
 .menu {
   display: flex;
-
 }
 
 .menu-container {
@@ -103,17 +96,17 @@ export default defineComponent({
 }
 
 .menu ul li img.logo {
-margin-right: 10px; /* Ajusta el espacio entre el logo y el texto */
+  margin-right: 10px; /* Ajusta el espacio entre el logo y el texto */
 }
 
-.logo{
+.logo {
   height: auto; /* Mantén la proporción */
-  width: 50px;  /* Ajusta el tamaño según lo necesario */
+  width: 50px; /* Ajusta el tamaño según lo necesario */
   object-fit: contain; /* Evita que el logo se distorsione */
   margin-top: 10;
 }
 
-.menu ul li h1{
+.menu ul li h1 {
   margin-top: -6px;
 }
 .menu ul li {
@@ -122,7 +115,7 @@ margin-right: 10px; /* Ajusta el espacio entre el logo y el texto */
   height: 60px; /* Asegúrate de que tengan la misma altura que el navbar */
   display: flex;
   align-items: center;
-}  
+}
 .menu ul li a {
   text-decoration: none;
   color: white;
@@ -131,28 +124,27 @@ margin-right: 10px; /* Ajusta el espacio entre el logo y el texto */
 }
 
 .menu ul li a:hover {
-  color: #A3B18A;
+  color: #a3b18a;
   margin-top: 4px;
 }
 
-.menu h3{
+.menu h3 {
   margin-top: 10px;
   font-size: 1.2rem;
 }
 
-
 .auth-buttons button {
-background-color: #507757;
-color: white;
-border: none;
-padding: 6px 10px;
-border-radius: 20px;
-cursor: pointer;
-transition: background-color 0.4s;
-font-size: 0.9rem;
+  background-color: #507757;
+  color: white;
+  border: none;
+  padding: 6px 10px;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: background-color 0.4s;
+  font-size: 0.9rem;
 }
 
-.auth-buttons{
+.auth-buttons {
   margin-left: 100px;
 }
 
@@ -160,7 +152,4 @@ font-size: 0.9rem;
   background-color: #23352c;
   margin-top: 2px;
 }
-
 </style>
-
-
