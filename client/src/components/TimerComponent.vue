1751b1b685ce84a98d8c2d725970e87bf523c98d
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-12-04 23:19:52 +0000

    timer logic fixed for duration

--- client/src/components/TimerComponent.vue
+++ client/src/components/TimerComponent.vue
@@ -1,19 +1,20 @@
 <template>
-    <div>
-        <h1>Timer</h1>
-        <p>{{ timeDisplay }}</p>
-        <button @click="startTimer(workDuration, breakDuration)">Iniciar</button>
-        <button @click="pauseTimer">Pausar</button>
-        <button @click="stopTimer">Detener</button>
+    <div class="timer-component">
+        <h1 class="timer-title">Timer</h1>
+        <p class="timer-display">{{ timeDisplay }}</p>
+        <button class="timer-button" @click="startTimer(workDuration, breakDuration)">Start</button>
+        <button class="timer-button" @click="pauseTimer">Pause</button>
+        <button class="timer-button" @click="stopTimer">Restart</button>
     </div>
 </template>
 
 <script lang="ts">
-import { useTimerStore } from '@/stores/timer_store'
-import { defineComponent, onMounted, computed } from 'vue'
-// import { useMethodsStore } from '@/stores/methods_store'
-import { usePreferencesStore } from '@/stores/userPreferences_store'
 import { useAuthStore } from '@/stores/authstore'
+import { useTimerStore } from '@/stores/timer_store'
+import { defineComponent, onMounted, computed, ref } from 'vue'
+import { usePreferencesStore } from '@/stores/userPreferences_store'
+
+
 import { Types } from 'mongoose'
 export default defineComponent({
     name: 'TimerComponent',
@@ -21,20 +22,24 @@ export default defineComponent({
         const timerStore = useTimerStore();
         const authStore = useAuthStore();
         const preferencesStore = usePreferencesStore();
-        
-        
         const userIDString = authStore.loggedUserId
         const userID = new Types.ObjectId(userIDString)
         const timeDisplay = computed(() => timerStore.timeDisplay)
+        const workDuration = ref(0);
+        const breakDuration = ref(0);
+        const methodName = ref('');
         onMounted(async () => {
-            await preferencesStore.getUserPreferences(userID)
-        });
-        const workDuration = computed(() => {
-            return preferencesStore.userPreferences.length > 0 ? preferencesStore.userPreferences[0].workDuration : 0;
-        });
+            try {
+                await preferencesStore.getUserPreferences(userID);
+                if (preferencesStore.userPreferences.length > 0) {
+                    const preferences = preferencesStore.userPreferences[0];
+                    workDuration.value = preferences.workDuration;
+                    breakDuration.value = preferences.breakDuration;
+                }
 
-        const breakDuration = computed(() => {
-            return preferencesStore.userPreferences.length > 0 ? preferencesStore.userPreferences[0].breakDuration : 0;
+            } catch (error) {
+                console.error('Error al obtener las preferencias del usuario:', error);
+            }
         });
         const startTimer = (workDuration: number, breakDuration: number) => {
             timerStore.startTimer(workDuration, breakDuration)
@@ -53,8 +58,39 @@ export default defineComponent({
             workDuration,
             breakDuration,
             pauseTimer,
-            stopTimer
+            stopTimer,
+            methodName
         }
     } 
 })
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+
+.timer-title {
+    font-family: 'Lobster', cursive;
+    font-size: 2rem;
+}
+
+.timer-button {
+    width: 50px;
+    font-size: 0.9rem;
+    background-color: #4c7761; 
+    color: white;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.6s;
+    border-radius: 50px;
+    margin-right: 10px;
+}
+
+button:hover  {
+    background-color: #344E41;
+    cursor: not-allowed;
+    border-color: #83af82;
+    box-shadow: 0px 0px 4px #79ac92;
+}
+
+</style>
+
