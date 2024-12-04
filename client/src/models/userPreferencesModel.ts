c9834483f5ae35465d5c38e9429ec572f6b61038
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-12-04 09:58:40 +0000

    adding dates objects to model

--- client/src/models/userPreferencesModel.ts
+++ client/src/models/userPreferencesModel.ts
@@ -1,19 +1,23 @@
-import { Types } from 'mongoose';
+import { Types } from 'mongoose'
 
 export interface UserPreferencesInterface {
-    userId: Types.ObjectId;
-    methodId: Types.ObjectId;
-    workDuration?: number;
-    breakDuration?: number;
-    createdAt: Date;
+    userId: Types.ObjectId
+    methodId: Types.ObjectId
+    workDuration: number
+    breakDuration: number
+    createdAt: Date
+    updatedAt: Date
+    _id?: Types.ObjectId
 }
 
 export class UserPreferences implements UserPreferencesInterface {
     constructor(
         public userId: Types.ObjectId,
         public methodId: Types.ObjectId,
-        public workDuration?: number,
-        public breakDuration?: number,
-        public createdAt: Date = new Date()
+        public workDuration: number,
+        public breakDuration: number,
+        public createdAt: Date = new Date(),
+        public updatedAt: Date = new Date(),
+        public _id?: Types.ObjectId,
     ) {}
 }
