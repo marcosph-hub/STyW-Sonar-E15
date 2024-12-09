074fe6221d151bc1b24a382927457e9b111ca22d
Marcos Padilla Herrera <marcosph98@gmail.com>
2024-12-09 18:16:40 +0000

    user subject creaton implemented

--- client/src/components/SubjectCreator.vue
+++ client/src/components/SubjectCreator.vue
@@ -1,59 +1,117 @@
 <template>
     <div class="subject-creator">
-        <h2>Crear Asignatura</h2>
-        <label for="subject-name">Nombre de la Asignatura:</label>
-        <input type="text" id="subject-name" v-model="subjectName" placeholder="Introduce el nombre de la asignatura" />
+        <h2>Which subject are you going to study?</h2>
+        <label for="subject-name">Subject name:</label>
+        <input type="text" id="subjectName" v-model="newSubject.name" placeholder="Name of the subject you want to study" />
 
-        <label for="subject-description">Descripción de la Asignatura:</label>
-        <input type="text" id="subject-description" v-model="subjectDescription" placeholder="Introduce la descripción de la asignatura" />
+        <label for="subject-description">Subject description:</label>
+        <input type="text" id="subjectDescription" v-model="newSubject.description" placeholder="Introduce the description of the subject" />
 
-        <button @click="saveSubject">Guardar Asignatura</button>
+        <button @click="saveSubject">Lets start studying!!</button>
     </div>
 </template>
 
 <script lang="ts">
-import { Types } from 'mongoose';
+// import { Types } from 'mongoose';
 import { defineComponent, /*onMounted,*/ ref } from 'vue';
 import { useSubjectStore } from '@/stores/subjects_store';
 import { usePreferencesStore } from '@/stores/userPreferences_store';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
     name: 'SubjectCreator',
     setup() {
-        const subjectName = ref('');
-        const subjectDescription = ref('');
-        const { addSubject } = useSubjectStore();
-        const preferencesStore = usePreferencesStore();
 
-        async function saveSubject() {
-            if (subjectName.value && subjectDescription.value) {
-                const newSubject = {
-                    name: subjectName.value,
-                    description: subjectDescription.value,
-                    type_education: 'default'
-                };
-                addSubject(newSubject);
-                subjectName.value = '';
-                subjectDescription.value = '';
-            } else {
-                alert('Please fill in all fields');
-            }
-            
-            const userID = sessionStorage.getItem('userID');
-            const methodID = sessionStorage.getItem('methodID');
-            console.log('userID', userID)
-            console.log('methodID', methodID)
-            if (userID && methodID) {
-                const userIDObject = new Types.ObjectId(userID);
-                const methodIDObject = new Types.ObjectId(methodID);
-                await preferencesStore.addUserPreferences(userIDObject, methodIDObject)
-            }
+        const subjectStore = useSubjectStore();
+        const preferencesStore = usePreferencesStore();
+        const router = useRouter();
+        const newSubject = ref({
+            name: '',
+            description: ''
+        });
+        const saveSubject = async () => {
+            const subject = await subjectStore.addSubject(newSubject.value)
+            sessionStorage.setItem('subjectName', subject.name)
+            savePreferences(subject._id)
         }
-        return { subjectName, subjectDescription, saveSubject }
+
+        const savePreferences = async (subjectId: string ) => {
+            const userID = sessionStorage.getItem('userID');
+            const methodId = sessionStorage.getItem('methodID');
+            
+
+            if(!userID || !methodId) {
+                throw new Error('User ID or method ID not found');
+            }
+            await preferencesStore.addUserPreferences(userID, methodId, subjectId)
+            router.push('/timer')
+
+        }
+        return { 
+            newSubject, 
+            saveSubject 
+        }
     }
 })
 </script>
 
 <style scoped>
+
+.subject-creator h2{
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+label {
+  display: block;
+  text-align: center;
+  margin-bottom: 1rem;
+  font-weight: bold;
+  color: #83af82;
+}
+
+input {
+  width: 100%; 
+  padding: 0.75rem;
+  margin-bottom: 2rem;
+  border-color: none;
+  border-radius: 10px;
+  font-size: 1rem;
+  background-color: #353e49;
+  outline: none;
+  border: none;
+  box-shadow: none;
+  transition: box-shadow 0.3s ease-in-out;
+}
+
+input:focus {
+  border-color: #83af82;
+  box-shadow: 0px 0px 4px #79ac92;
+  outline: none;
+  color: #83af82;
+  background-color: #282f38;
+  box-shadow: 0 0 5px #83af82
+}
+
+button {
+  width: 40%;
+  padding: 10px;
+  font-size: 1rem;
+  background-color: #344E41; 
+  color: white;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.4s;
+  border-radius: 50px;
+  text-align: center;
+  margin-top: 2rem;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+button:hover {
+  background-color: #23352c;
+}
 
 </style>
