7167b8fb6110c023222a23f21b39ee1b7192e5ad
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-12-11 12:26:44 +0000

    implementing cypress conmfig

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'cypress';
-
+import codeCoverageTask from '@cypress/code-coverage/task';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173', // Cambia según tu servidor
-    supportFile: false,
+    supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     pageLoadTimeout: 120000,
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config);
+      return config;
+    },
   },
   component: {
     devServer: {
--- client/cypress/support/e2e.ts
+++ client/cypress/support/e2e.ts
@@ -1,23 +1,15 @@
+import '@cypress/code-coverage/support';
+
 // Importa comandos personalizados (si existen)
 import './commands';
 
-
 // Manejar errores globales en Cypress
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // Ignora errores no controlados específicos si no afectan las pruebas
+Cypress.on('uncaught:exception', (err) => {
   console.log('Error capturado:', err.message);
   return false; // Evita que el test falle
 });
 
-
 // Configuración para todos los tests
 beforeEach(() => {
-  // Configuración que se ejecutará antes de cada prueba
   cy.log('Iniciando prueba...');
 });
-
-
-
-
-
-
