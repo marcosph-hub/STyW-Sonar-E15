4b86168535e9af16cfff97d1440b0c32b63118e0
Marcos Padilla Herrera <alu0101045177@ull.edu.es>
2024-12-11 12:27:12 +0000

    implementing jest conmfig

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -3,9 +3,18 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest', // Usa ts-jest como preset
   testEnvironment: 'node', // Entorno para pruebas en Node.js
-  verbose: true,          // Muestra resultados detallados
+  verbose: true, // Muestra resultados detallados
   moduleFileExtensions: ['ts', 'js'], // Archivos soportados
   testMatch: ['**/*.test.ts'], // Busca archivos .test.ts
+  collectCoverage: true, // Habilita la recolección de cobertura
+  coverageDirectory: 'coverage', // Directorio donde se guardan los reportes
+  coverageReporters: ['lcov', 'text'], // Genera cobertura en formato lcov y texto
+  collectCoverageFrom: [ // Archivos a incluir en el análisis de cobertura
+    'src/**/*.ts', // Incluye todos los archivos .ts en la carpeta src
+    '!src/**/*.d.ts', // Excluye definiciones de tipo
+    '!src/**/__tests__/**', // Excluye archivos de pruebas si están en una carpeta __tests__
+    '!src/main.ts', // Excluye archivos específicos si es necesario
+  ],
 };
 
 export default config;
